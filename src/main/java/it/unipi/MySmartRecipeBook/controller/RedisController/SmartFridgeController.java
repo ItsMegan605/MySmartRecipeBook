d9eeb3d152e8716dccb43440b46f470c3a6269c9
ItsMegan605 <meganmaremmani13@gmail.com>
2026-02-10 11:22:40 +0100

    smartfridge updates

--- src/main/java/it/unipi/MySmartRecipeBook/controller/RedisController/SmartFridgeController.java
+++ src/main/java/it/unipi/MySmartRecipeBook/controller/RedisController/SmartFridgeController.java
@@ -5,6 +5,7 @@ import it.unipi.MySmartRecipeBook.service.SmartFridgeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import it.unipi.MySmartRecipeBook.model.*;
 
 @RestController
 @RequestMapping("/api/fridge")
